3cc4dd39b51dc7ad85a6e1d47404c95bf711e024
Milton Valenzuela <miltvalenz@hotmail.com>
2020-10-23 19:51:02 -0400

    First commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Milton Valenzuela on 10/17/20.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Milton Valenzuela on 10/17/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var alertIsVisible: Bool = false
+    
+    var body: some View {
+        VStack {
+            Text("epale!!!")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(Color.pink)
+                .padding()
+            Button(action: {
+                print("Button pressed!!!")
+                self.alertIsVisible = true
+            }) {
+                Text("hit me")
+            }
+            .alert(isPresented: $alertIsVisible) { () ->
+                Alert in
+                return Alert(title: Text("Hello there!"), message: Text("This is my first pop-up."), dismissButton: .default(Text("Awesome!")))
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
